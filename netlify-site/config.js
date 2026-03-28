094e1b3b5b6f1f50b1ff896101ed68905e90f537
Frfayzullin <155719209+Frfayzullin@users.noreply.github.com>
2026-03-29 00:20:07 +0300

    Delete netlify-site directory

--- netlify-site/config.js
+++
@@ -1,6 +0,0 @@
-window.NETLIFY_APP_CONFIG = {
-  // Вставь сюда URL твоего Apps Script web app вида:
-  // https://script.google.com/macros/s/.../exec
-  apiBaseUrl: "/.netlify/functions/apps-script-proxy",
-  useTextPlainJson: true
-};

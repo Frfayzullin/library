97a02e1e2991c76739ebce4c31a08d5126ccc9d9
Frfayzullin <155719209+Frfayzullin@users.noreply.github.com>
2026-03-29 00:19:10 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.NETLIFY_APP_CONFIG = {
+  // Вставь сюда URL твоего Apps Script web app вида:
+  // https://script.google.com/macros/s/.../exec
+  apiBaseUrl: "/.netlify/functions/apps-script-proxy",
+  useTextPlainJson: true
+};
